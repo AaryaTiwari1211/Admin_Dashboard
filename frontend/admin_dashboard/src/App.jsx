5833c82958f949bbf2942d9b190cf4d88028f390
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-03-07 20:10:56 +0530

    Added theme colors and necessary data

--- frontend/admin_dashboard/src/App.jsx
+++ frontend/admin_dashboard/src/App.jsx
@@ -1,9 +1,14 @@
-
 import './App.css'
-
+import React from 'react'
+import { BrowserRouter, Routes , Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
