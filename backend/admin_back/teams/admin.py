f7ee0a23f016ca3ff7c039cb7c3ba026feec7310
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-08-18 00:47:06 +0530

    Removed Backend

--- backend/admin_back/teams/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Team
-
-class TeamAdmin(admin.ModelAdmin):
-    list_display = ("name", "age", "email",'phone','access_level')
-
-admin.site.register(Team, TeamAdmin)
--- backend/admin_back/teams/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .serializers import TeamSerializer
-from .models import Team
-from rest_framework import viewsets
-
-class TeamView(viewsets.ModelViewSet):
-    serializer_class = TeamSerializer
-    queryset = Team.objects.all()
-
